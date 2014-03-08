a689f0c603222f1bbf2389984bf263b05684450e
mrmikemcguire <mcguire@mullenhigh.com>
2014-03-08 09:24:25 -0700

    Initial working version

--- Player Account Tracker/src/Players.java
+++ Player Account Tracker/src/Players.java
@@ -6,51 +6,36 @@ public class Players
 	public static int numberOfPlayers;
 	private static String playerName;
 	private static int initialAmount;
-	
-	Players(String n,int ia)
-		{
-		playerName = n;
-		initialAmount = ia;
-		}
-	
+
 	public static void setUpPlayers()
 		{
 		System.out.println("How many players are there? ");
 		Scanner userInput1 = new Scanner(System.in);
 		int numberOfPlayers = userInput1.nextInt();
-		
-		Players[] players = new Players[numberOfPlayers];
+
+		Player[] players = new Player[numberOfPlayers];
 
 		Scanner userInput2 = new Scanner(System.in);
 
-		
 		for (int i = 0;i < numberOfPlayers;i++)
 			{
 			System.out.println("Please input the name of player number "+ (i + 1));
+
 			playerName = userInput2.nextLine();
 			initialAmount = PlayerAccounts.initialAmount;
 			
-			Players individual = new Players(playerName,initialAmount);
+			Player individual = new Player(playerName,initialAmount);
 			players[i] = individual;
-			
-			System.out.println("The player is " + players[i].playerName);
-			
-			System.out.println("The player named " + players[i].playerName + " has $" + players[i].initialAmount);
 			}
-		
-		System.out.println("Player #1 is named " + players[0].playerName);
-		System.out.println("Player #2 is named " + players[1].playerName);
 
 		System.out.println();
 		System.out.println("The " + numberOfPlayers + " players and their starting amounts are:");
 		System.out.println();
-		
+
 		for (int j = 0;j <numberOfPlayers;j++)
 			{
-			System.out.println("Player # " + (j + 1) + " is named " + players[j].playerName);
-
-			System.out.println(players[j].playerName);
-			System.out.println(players[j].initialAmount);
+			System.out.println("Player # " + (j + 1) + " is named " + players[j].getName() +
+					" and has $" + players[j].getCurrentAmount());
 			}
 		}
 	}
